1c2b57d4d32c4a4cae40403444958554347deae7
Ivan Kazmenko <117230564+I1Kazma@users.noreply.github.com>
2025-02-22 16:27:57 +0300

    Add files via upload

--- send_mail.php
+++ send_mail.php
@@ -2,8 +2,15 @@
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $comment = $_POST['comment'];
-$to = 'karmakazmenko@gmail.com';
-$header = 'Отзыв от пользователя';
+$to = '89851007873@mail.ru';
+$header = 'Новая заявка на сайте';
 $message = "Имя пользователя: $name \nНомер телефона: $phone \nОтзыв: $comment";
 $send = mail($to, $header, $message, "Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
+if (mail($to, $subject, $message, $headers)) {
+    header('Location: success.html');
+    exit();
+} else {
+    header('Location: index.html');
+    exit();
+}
 ?>
